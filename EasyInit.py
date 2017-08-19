82289a66b765fd60153afc3da243529e9b1107db
BerryPi <naturalneopoint@gmail.com>
2017-08-19 14:21:18 -0400

    Added option to read player names and initiative bonuses from a file, and to give enemies an initiative bonus.

--- EasyInit.py
+++ EasyInit.py
@@ -1,23 +1,46 @@
+import argparse
 import random
+
+# Check if a list was specified
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file",
+    help="A comma-separated file containing characters to automatically roll for and their initiative bonuses")
+args = parser.parse_args()
+
 # Initialising the initiatives array
 initiatives = []
-# Request a name and predetermined initiative roll until a blank name is entered
-while (True):
-    name = input("Enter a character name, or press enter to move on: ")
-    if (name == ""):
-        break
-    init = int(input("Enter this character's initiative roll: "))
-    initiatives.append((name, init))
+
+# If a file was specified, open it.
+if (args.file):
+    with open(args.file, newline='') as init_file:
+        for line in init_file:
+            # Split the line into the character name and initiative bonus
+            line_info = line.split(",")
+            name = line_info[0]
+            init = random.randint(1, 21) + int(line_info[1])
+            initiatives.append((name, init))
+
+# If not, prompt the user to input the values
+else:
+    # Request a name and predetermined initiative roll until a blank name is entered
+    while (True):
+        name = input("Enter a character name, or press enter to move on: ")
+        if (name == ""):
+            break
+        init = int(input("Enter this character's initiative roll: "))
+        initiatives.append((name, init))
 
 # Setting up the generic enemies, using the same system as above
 while (True):
     name = input("Enter a generic enemy name, or press enter to move on: ")
     if (name == ""):
         break
+    # Get an initiative bonus for enemies
+    enemy_bonus = int(input("Enter the enemies' initiative bonus: "))
     qty = int(input("How many of these enemies to create: "))
     for i in range(qty):
         curr_name = name + " " + str(i+1)
-        curr_init = random.randint(1, 21)
+        curr_init = random.randint(1, 21) + enemy_bonus
         initiatives.append((curr_name, curr_init))
 
 # Sort the list
@@ -26,3 +49,4 @@ initiatives.sort(key=lambda tup: tup[1], reverse=True)
 # Pretty-print the list
 for pair in initiatives:
     print(str(pair[0]) + "\t" + str(pair[1]))
+
--- README.md
+++ README.md
@@ -2,8 +2,16 @@
 A tool for Tabletop GMs to more easily determine initiative orders in encounters with multiple copies of an enemy.
 
 ## Usage
-Have your players roll for initiative as normal.
-When prompted, type in their names and the results of their rolls (after applying any bonuses)
+There are two ways to use this script.
+
+The first way is to run the script with no command line options specified, have your players roll for initiative as normal. When prompted, type in their names and the results of their rolls (after applying any bonuses).
 Once all players have been entered in, press Enter at the prompt.
-Now, enter the name of the enemy you want to add to your encounter, for example 'Guard' or 'Bandit', and their quantity.
+
+The second is to store each player's name and their initiative bonus in a file, one player per line, in this format:
+
+`Player Name,Bonus`
+
+Then run the script with the `--file, -f` option and the name of this file. The script will automatically roll a d20 for initiative for each player and apply their bonus.
+
+Now, enter the names of the enemy you want to add to your encounter, for example 'Guard' or 'Bandit', their initiative bonus, and their quantity.
 The script will automatically roll a d20 for initiative for each one of the enemies, then display a list, sorted in descending order, of each character and their initiatives.
